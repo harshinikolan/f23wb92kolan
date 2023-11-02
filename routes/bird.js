0418fd74b2a07f43bb503e187532d027a294ebf0
harshinikolan <142836121+harshinikolan@users.noreply.github.com>
2023-11-01 23:41:41 -0500

    added

--- routes/bird.js
+++ routes/bird.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('bird', { title: 'bird Results' });
+  res.render('bird', { title: 'Search results bird' });
 });
 
 module.exports = router;
