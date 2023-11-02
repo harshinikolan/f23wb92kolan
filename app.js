d42bbb9bb46379e91346113e27c6c2bba4fa6148
harshinikolan <142836121+harshinikolan@users.noreply.github.com>
2023-11-01 22:32:42 -0500

    added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var birdRouter = require('./routes/bird');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bird', birdRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 
 // catch 404 and forward to error handler
