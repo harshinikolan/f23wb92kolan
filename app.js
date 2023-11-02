7510bf7da5918e5dbe8ef34939e99735ced699ed
harshinikolan <142836121+harshinikolan@users.noreply.github.com>
2023-11-01 21:44:26 -0500

    added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var birdRouter = require('./routes/bird');
+var boardRouter = require('./routes/board');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bird', birdRouter);
+app.use('/board', boardRouter);
 
 
 // catch 404 and forward to error handler
